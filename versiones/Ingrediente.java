1aaed36940a3aa584b024dec855ddd596778f25b
Manuel Quinto <maquimed@gmail.com>
2017-04-03 16:18:28 +0200

    toString en Ingrediente

--- versiones/Ingrediente.java
+++ versiones/Ingrediente.java
@@ -27,6 +27,10 @@ public class Ingrediente {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	@Override
+	public String toString() {
+		return cantidad + " " + unidad + " de " + nombre;
+	}
 	
 	
 }
