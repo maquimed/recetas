93aaea373fc0766cc330b2feb2815735256cb42e
Manuel Quinto <maquimed@gmail.com>
2017-04-03 13:01:31 +0200

    Añadidos atributos, constructores, getters y setters.
    Signed-off-by: Manuel Quinto <maquimed@gmail.com>

--- versiones/Recetas.java
+++ versiones/Recetas.java
@@ -1,5 +1,34 @@
 package versiones;
 
 public class Recetas {
-
+	private String nombre;
+	private String ingredientes;
+	private String preparacion;
+	public Recetas(String nombre) {
+		this.nombre = nombre;
+	}
+	public Recetas(String nombre, String ingredientes, String preparacion) {
+		this.nombre = nombre;
+		this.ingredientes = ingredientes;
+		this.preparacion = preparacion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getIngredientes() {
+		return ingredientes;
+	}
+	public void setIngredientes(String ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+	public String getPreparacion() {
+		return preparacion;
+	}
+	public void setPreparacion(String preparacion) {
+		this.preparacion = preparacion;
+	}
+	
 }
